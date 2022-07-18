2958912f1e9973580d43402fadfe7f671d2b7a74
phillydam <phillydam@gmail.com>
2022-07-18 14:43:51 -0400

    setup characters

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
+// typical character setup
 class Character {
-    constructor(name, health = 100){
+    constructor(name, attacks = [], health = 100, endurance = 100){
         this.name = name
+        this.attacks = attacks
         this.health = health
+        this.endurance = endurance
     }
-
-
 }
+
+// class Player1 extends Character {
+//     constructor(){
+//         super(name, health, attacks)
+//     }
+// }
+
+
+// Setup character options
+const airBender = new Character('Aang', ['Kick', 'Wind Slash', 'Tornado'])
+const earthBender = new Character('Toph', ['Punch', 'Stomp', 'Earthquake'])
+const fireBender = new Character('Zuko', ['Slash', 'Fireball', 'Lightning'])
+const waterBender = new Character('Katara', ['Slap', 'Water Whip', 'Tsunami'])
+
+console.log(airBender)
+console.log(earthBender)
+console.log(fireBender)
+console.log(waterBender)
