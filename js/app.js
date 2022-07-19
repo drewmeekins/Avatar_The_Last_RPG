a3a42f6d4e7f05b59160df20352d1031074e1134
phillydam <phillydam@gmail.com>
2022-07-18 20:48:20 -0400

    images added

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // typical character setup
 class Character {
-    constructor(name, attacks = [], health = 100, endurance = 100){
+    constructor(name, attacks = [], element, health = 100, endurance = 100){
         this.name = name
         this.attacks = attacks
+        this.element = element
         this.health = health
         this.endurance = endurance
     }
@@ -16,10 +17,10 @@ class Character {
 
 
 // Setup character options
-const airBender = new Character('Aang', ['Kick', 'Wind Slash', 'Tornado'])
-const earthBender = new Character('Toph', ['Punch', 'Stomp', 'Earthquake'])
-const fireBender = new Character('Zuko', ['Slash', 'Fireball', 'Lightning'])
-const waterBender = new Character('Katara', ['Slap', 'Water Whip', 'Tsunami'])
+const airBender = new Character('Aang', ['Kick', 'Wind Slash', 'Tornado'], 'air')
+const earthBender = new Character('Toph', ['Punch', 'Stomp', 'Earthquake'], 'earth')
+const fireBender = new Character('Zuko', ['Slash', 'Fireball', 'Lightning'], 'fire')
+const waterBender = new Character('Katara', ['Slap', 'Water Whip', 'Tsunami'], 'water')
 
 console.log(airBender)
 console.log(earthBender)
