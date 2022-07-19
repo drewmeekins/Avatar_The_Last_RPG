71329ee061b84cae1fe47dd94c44da866c491359
drewmeekins <drew.a.meekins@gmail.com>
2022-07-19 13:06:28 -0400

    beginning game parameters

--- js/app.js
+++ js/app.js
@@ -1,5 +1,25 @@
+// Setup Game
+const playGame = {
+    startGame(name) {
+        this.restart(name)
+        this.characterSelect(name)
+    },
+
+    restart(name) {
+        
+    },
+
+    characterSelect(name) {
+
+    }
+}
+
+
+let player1
+let player2
+
 // typical character setup
-class Character {
+class Player1 {
     constructor(name, attacks = [], element, health = 100, endurance = 100){
         this.name = name
         this.attacks = attacks
@@ -7,6 +27,26 @@ class Character {
         this.health = health
         this.endurance = endurance
     }
+
+    playerAttack1() {
+        return this.health - this.attacks[0]
+    }
+
+}
+
+class Player2 {
+    constructor(name, attacks = [], element, health = 100, endurance = 100){
+        this.name = name
+        this.attacks = attacks
+        this.element = element
+        this.health = health
+        this.endurance = endurance
+    }
+
+    playerAttack1() {
+        return this.health - this.attacks[0]
+    }
+
 }
 
 // class Player1 extends Character {
@@ -17,10 +57,10 @@ class Character {
 
 
 // Setup character options
-const airBender = new Character('Aang', ['Kick', 'Wind Slash', 'Tornado'], 'air')
-const earthBender = new Character('Toph', ['Punch', 'Stomp', 'Earthquake'], 'earth')
-const fireBender = new Character('Zuko', ['Slash', 'Fireball', 'Lightning'], 'fire')
-const waterBender = new Character('Katara', ['Slap', 'Water Whip', 'Tsunami'], 'water')
+const airBender = new Player1('Aang', ['Kick', 'Wind Slash', 'Tornado'], 'air')
+const earthBender = new Player1('Toph', ['Punch', 'Stomp', 'Earthquake'], 'earth')
+const fireBender = new Player1('Zuko', ['Slash', 'Fireball', 'Lightning'], 'fire')
+const waterBender = new Player1('Katara', ['Slap', 'Water Whip', 'Tsunami'], 'water')
 
 console.log(airBender)
 console.log(earthBender)
